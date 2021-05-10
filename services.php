e74dfaf601e57464f59e80a1205c0824613bdb5f
tumwine <tumwineivan20@gmail.com>
2021-05-10 13:05:09 +0300

    Update services.php

--- services.php
+++ services.php
@@ -75,16 +75,16 @@
       <div class="brief-info">
         <h4><?php echo $service["name"]; ?></h4>
         <div class="service-info">
-          <?php echo html_entity_decode(substr($service["body"], 0, 200) . "..."); ?> 
-        </div>
-      </div>
-      <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+      <div>
+        <a href="services-info.php?id=<?php echo $service["id"]?>">Read more >></a>
       </div>
     </div>
   <?php endforeach; ?>
