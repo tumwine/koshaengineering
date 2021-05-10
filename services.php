37c248b06b304ef07ed21d0b9a945c1022a7e2a6
tumwine <tumwineivan20@gmail.com>
2021-05-10 13:03:49 +0300

    Update services.php

--- services.php
+++ services.php
@@ -79,7 +79,12 @@
         </div>
       </div>
       <div>
-        <a href="services-info.php?id=<?php echo $service["id"]?>">Read more >></a>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
     </div>
   <?php endforeach; ?>
